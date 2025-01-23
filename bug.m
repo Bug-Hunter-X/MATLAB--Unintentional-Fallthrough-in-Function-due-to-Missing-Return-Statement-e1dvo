2d5b66f0d5788a9742708d47bb334a3bdfb8bfa2
BugHunter-X <askzenai@gmail.com>
2025-01-23 21:52:05 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,8 @@
+function result = myFunction(input)
+  % Some code here
+  if input < 0
+    result = -1;  % Handle negative input
+    return;       % Important: Return here!
+  end
+  % ... rest of the function code
+end
